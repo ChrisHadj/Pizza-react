8ea963feda6372c27402bf958dc77c4c5aa3d2c5
ChrisHadj <60141623+ChrisHadj@users.noreply.github.com>
2021-01-28 13:12:46 +0200

    update router

--- pizzeriareact/src/index.js
+++ pizzeriareact/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Menu from "./components/Pages/Menu/index";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, HashRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/Menu" component={Menu} />
     </Switch>
-  </BrowserRouter>,
+  </HashRouter>,
   rootElement
 );
